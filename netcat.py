fd628a447d59a617a6a92c87d54d2ac9eef4fdee
László Balla-Kovács <balla.kovacs.laszlo@gmail.com>
2016-06-05 15:07:24 +0200

    Create netcat.py

---
+++ netcat.py
@@ -0,0 +1,17 @@
+import subprocess, time, socket
+
+def netcat(hostname, port):    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+    s.bind((hostname, port))
+    s.listen(5)    
+    while True:
+        c, addr = s.accept()                
+        c.send(core_temp())
+        c.close()
+
+def core_temp():
+    core_temp = subprocess.check_output( \
+    'sensors | grep "Core 0:" | cut -d"+" -f2 | cut -c 1-4', shell=True)
+    #.strip()
+    #.decode('ascii')
+    return core_temp
